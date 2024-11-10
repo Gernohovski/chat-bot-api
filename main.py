dd4f45b60b9ae969c7936b2f4de5d93c788efbc7
Gernohovski <gernohovskirodrigo@gmail.com>
2024-11-10 13:47:59 -0300

    feat: Adição do mapeamento de cors para consumo da API no front-end

--- main.py
+++ main.py
@@ -6,6 +6,18 @@ from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
+
 class TextRequest(BaseModel):
     text: str
 
